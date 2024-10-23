5a2c5011ba1b9c8a129e525a170c5d7a4009323d
Hadiahadia <mehayechhadia@gmail.com>
2024-10-23 18:29:59 +0100

    Create tp1.c

---
+++ tp1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){int s[5][5];
+int i,j;
+for(i=0;i<5;i++){
+for(j=0;j<5;j++){
+printf("enter value of s[%d][%d]
+  scanf("%d",s[i][j];
+}
+}
+printf("\n");
+return 0;
+
+
+
+
+
+
+#include<stdio.h>
+int main(){int a[5][5];
+int i;
+for(i=0;i<5;i++){
+cin>=a[i]
+  for(i=0;i<5;i++)
+  if(a[i]%2==0)
+printf("%d",a[i])
+printf("\n");
+return 0;
